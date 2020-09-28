7f136c91c50568c5bef95a689c9687851b754226
kyd <husheart@naver.com>
2020-09-29 03:28:25 +0900

    1 - 회원 도메인 실행과 테스트
    스프링 핵심 원리 이해 1 - 예제만들기

---
+++ src/main/java/tk/youngdk/spring_corebasic/MemberApp.java
@@ -0,0 +1,30 @@
+package tk.youngdk.spring_corebasic;
+
+import tk.youngdk.spring_corebasic.domain.member.Grade;
+import tk.youngdk.spring_corebasic.domain.member.Member;
+import tk.youngdk.spring_corebasic.service.MemberService;
+import tk.youngdk.spring_corebasic.service.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+
+        MemberService memberService = new MemberServiceImpl();
+
+        Member m1 = new Member(1L, "m1", Grade.VIP);
+
+        memberService.join(m1);
+
+
+        Member findMember = memberService.findMember(m1.getId());
+
+        System.out.println(m1.toString());
+        System.out.println(findMember.toString());
+
+        /*
+        * 애플리케이션 로직으로 이렇게 테스트 하는 것은 좋은 방법이 아니다. JUnit 테스트를 사용하자.
+        * */
+
+    }
+
+}
---
+++ src/test/java/tk/youngdk/spring_corebasic/member/MemberServiceTest.java
@@ -0,0 +1,33 @@
+package tk.youngdk.spring_corebasic.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import tk.youngdk.spring_corebasic.domain.member.Grade;
+import tk.youngdk.spring_corebasic.domain.member.Member;
+import tk.youngdk.spring_corebasic.service.MemberService;
+import tk.youngdk.spring_corebasic.service.MemberServiceImpl;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join(){
+        // given
+        Member m1 = new Member(1L, "m1", Grade.VIP);
+
+        memberService.join(m1);
+        Member findMember = memberService.findMember(m1.getId());
+
+        Assertions.assertThat(m1).isEqualTo(findMember);
+    }
+
+    /*
+    회원 도메인 설계의 문제점
+     - 이 코드의 설계상 문제점은 무엇일까요?
+     - 다른 저장소로 변경할 때 OCP 원칙을 잘 준수할까요?
+     - DIP를 잘 지키고 있을까요?
+     - 의존관계가 인터페이스 뿐만 아니라 구현까지 모두 의존하는 문제점이 있음
+     - 주문까지 만들고나서 문제점과 해결 방안을 설명
+    */
+}
