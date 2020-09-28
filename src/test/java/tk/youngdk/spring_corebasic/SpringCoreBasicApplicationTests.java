6cb18600366348fa3f913061bf3a7fabdfb9f5ea
kyd <husheart@naver.com>
2020-09-29 02:45:31 +0900

    spring-core basic init

---
+++ src/test/java/tk/youngdk/spring_corebasic/SpringCoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.youngdk.spring_corebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
