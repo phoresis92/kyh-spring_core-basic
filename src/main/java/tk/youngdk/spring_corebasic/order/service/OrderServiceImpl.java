eac1b924b4808df7e598f0392d261301c296e9df
kyd <husheart@naver.com>
2020-10-02 01:29:01 +0900

    6 - 생성자 주입을 선택해라!
    의존관계 자동 주입

--- src/main/java/tk/youngdk/spring_corebasic/order/service/OrderServiceImpl.java
+++ src/main/java/tk/youngdk/spring_corebasic/order/service/OrderServiceImpl.java
@@ -13,11 +13,6 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-//    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-
-    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
