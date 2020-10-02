343d9b16ca9be5f15d78d04a26d03c6c483f57b5
kyd <husheart@naver.com>
2020-10-02 12:56:46 +0900

    7 - 인터페이스 InitializingBean, DisposableBean
    빈 생명주기 콜백

---
+++ src/test/java/tk/youngdk/spring_corebasic/lifeCycle/NetworkClient.java
@@ -0,0 +1,46 @@
+package tk.youngdk.spring_corebasic.lifeCycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    public void setUrl(String url) {
+        System.out.println("NetworkClient.setUrl");
+        this.url = url;
+    }
+
+    //서비스 시작시 호출
+    public void connect(){
+        System.out.println("connect = " + url);
+    }
+
+    public void call(String message){
+        System.out.println("call = " + url + " message = " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect(){
+        System.out.println("close = " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // 의존관계 주입이 끝나면 호출
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
+}
